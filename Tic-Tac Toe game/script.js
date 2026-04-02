54698c2125cd07ab17e3cc8f87e1caf74d69fe34
Anamika Pandey <anamikapndey9305@gmail.com>
2026-04-02 11:23:19 +0530

    Save current changes before pull

--- Tic-Tac Toe game/script.js
+++ Tic-Tac Toe game/script.js
@@ -1,7 +1,9 @@
 let currentPlayer = "X";
 let arr = Array(9).fill(null);
 
-function checkWinner(){
+function checkWinner(){ // runs after every move
+
+
     if(
         (arr[0]!==null && arr[0] ==arr[1]&& arr[1]==arr[2]) ||
         (arr[3]!==null && arr[3] ==arr[4]&& arr[4]==arr[5]) ||
@@ -12,14 +14,15 @@ function checkWinner(){
         (arr[0]!==null && arr[0] ==arr[4]&& arr[4]==arr[8]) ||
         (arr[2]!==null && arr[2] ==arr[4]&& arr[4]==arr[6])
     ){
-        alert(`Winner is ${currentPlayer}`);
-        return;
+       document.getElementById("result").innerText = `Winner is ${currentPlayer}`; // return the winner
+       return;
     }
-
     if(!arr.some((e)=> e===null)){
-        alert(`Draw!!`);
-        return;
+        document.getElementById("result").innerText = "Draw!!"; // return draw
     }
+    
+
+   
 
            
 
@@ -29,16 +32,16 @@ function checkWinner(){
 
 }
 
+  
 
+function handleClick(el){ // runs whenever box is clicked
 
-function handleClick(el){
-
-    const id = Number(el.id);
-    if (arr[id]!== null) return ;
-    arr[id] = currentPlayer;
-    el.innerText = currentPlayer;
-    checkWinner();
-    currentPlayer = currentPlayer === "X"? "O":"X";
+    const id = Number(el.id); //Converts string "0" → number 0 , used to access array index
+    if (arr[id]!== null) return ; // preventing overwriting
+    arr[id] = currentPlayer; // save move in array
+    el.innerText = currentPlayer; // Displays X or O inside clicked box
+    checkWinner();  //After every move → check game result
+    currentPlayer = currentPlayer === "X"? "O":"X"; // toggle the turn
 
 
     
